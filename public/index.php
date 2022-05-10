b02df714ed4e36bc6a37db221caf184d3b7c0004
Samir Santos <santossamir@hotmail.com>
2022-05-09 23:02:54 -0300

    Iniciando a configuração de rotas.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
     require_once "../vendor/autoload.php";
+
+    $route = new \App\Route;
     echo "This is working!";
+    echo "<hr>";
+    echo "<pre>";
+    print_r($route->getUrl());
+    echo "</pre>";
 ?>
