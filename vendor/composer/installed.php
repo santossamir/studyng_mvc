f909d3b88939cb6fa670fa5fb53aec51bda8da4d
Samir Santos <santossamir@hotmail.com>
2022-05-09 22:34:06 -0300

    Instalando e incluindo o Composer no projeto, além de configurar a PSR-4 para o sistema de Autoload.4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1b8f2b3c0374f15efc2d721c6b910c0db994d176',
+        'name' => 'vendor-name/miniframework',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor-name/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1b8f2b3c0374f15efc2d721c6b910c0db994d176',
+            'dev_requirement' => false,
+        ),
+    ),
+);
