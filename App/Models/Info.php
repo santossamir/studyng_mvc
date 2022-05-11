62cdd7dfce202968b5da185a63edc0b6e4809399
Samir Santos <santossamir@hotmail.com>
2022-05-11 00:20:49 -0300

    Criando a abstração dos Models.

--- App/Models/Info.php
+++ App/Models/Info.php
@@ -1,14 +1,10 @@
 <?php
     namespace App\Models;
 
-    class Info {
+    use MF\Model\Model;
+
+    class Info extends Model {
         
-        protected $db;
-
-        public function __construct(\PDO $db){
-            $this->db = $db;
-        }
-
         public function getInfo(){
             $query = "select titulo, descricao from tb_info";
             return $this->db->query($query)->fetchAll();
--- App/Models/Produto.php
+++ App/Models/Produto.php
@@ -1,14 +1,10 @@
 <?php
     namespace App\Models;
 
-    class Produto {
+    use MF\Model\Model;
+
+    class Produto extends Model {
         
-        protected $db;
-
-        public function __construct(\PDO $db){
-            $this->db = $db;
-        }
-
         public function getProdutos(){
             $query = "select id, descricao, preco from tb_produtos";
             return $this->db->query($query)->fetchAll();
