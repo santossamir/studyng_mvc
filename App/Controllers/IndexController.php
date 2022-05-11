d285ea716335897c000ec968decf78c8ba5c499c
Samir Santos <santossamir@hotmail.com>
2022-05-10 21:45:03 -0300

    Desenvolvendo a abstração dos Controllers.

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -1,18 +1,13 @@
 <?php
     namespace App\Controllers;
 
-    class IndexController {
+    use MF\Controller\Action;
 
-        private $view;
-
-        public function __construct(){
-            $this->view = new \stdClass();
-        }
+    class IndexController extends Action {
 
         public function index(){
 
             $this->view->dados = ['Sofá', 'Cadeira', 'Cama'];
-            
             $this->render('index');
         }
 
@@ -23,16 +18,5 @@
 
         }
 
-        public function render($view) {
-
-            $classAtual = get_class($this);
-
-            $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
-
-            $classAtual = strtolower(str_replace('Controller', '', $classAtual));
-            
-            require_once "../App/Views/".$classAtual."/".$view.".phtml";
-        
-        }
     }
 ?>
